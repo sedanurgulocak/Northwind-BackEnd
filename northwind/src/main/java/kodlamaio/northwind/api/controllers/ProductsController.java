272a0dc1830348f3cd6eef5502626f24b0f9e48f
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-16 22:43:46 +0300

    🔨Update product controller according to result structure

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.entities.concretes.Product;
 
 @RestController
@@ -23,7 +24,7 @@ public class ProductsController {
 	}
 
 	@GetMapping("/getall")
-	public List<Product> getAll(){
+	public DataResult<List<Product>> getAll(){
 		return this.productService.getAll();
 	}
 }
