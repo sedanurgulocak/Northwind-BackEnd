e1a45e4f556e96436c3a44fd4ef9cdf1f2a14f41
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-21 23:52:53 +0300

    🔨Rename methods

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -11,17 +11,17 @@ public interface ProductDao extends JpaRepository<Product, Integer> {
 
 	Product getByProductName(String productName);
 	
-	Product getByProductNameAndCategoryId(String productName, int categoryId);
+	Product getByProductNameAndCategory_CategoryId(String productName, int categoryId);
 	
-	List<Product> getByProductNameOrCategoryId(String productName, int categoryId);
+	List<Product> getByProductNameOrCategory_CategoryId(String productName, int categoryId);
 	
-	List<Product> getByCategoryIdIn(List<Integer> categories);
+	List<Product> getByCategoryIn(List<Integer> categories);
 	
 	List<Product> getByProductNameContains(String productName);
 	
 	List<Product> getByProductNameStartsWith(String productName);
 	
-	@Query("From Product where productName=:productName and categoryId=:categoryId")
+	@Query("From Product where productName=:productName and category.categoryId=:categoryId")
 	List<Product> getByNameAndCategory(String ProductName, int categoryId);
 	
 	
