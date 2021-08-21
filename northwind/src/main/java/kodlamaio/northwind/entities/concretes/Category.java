e0718f15c6ace1d73484e61cfd0686e1869e40bb
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-21 23:52:07 +0300

    🔨Rename mappedBy

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Category.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Category.java
@@ -26,6 +26,6 @@ public class Category {
 	@Column(name="category_name")
 	private String categoryName;
 	
-	@OneToMany(mappedBy = "categories")
+	@OneToMany(mappedBy = "category")
 	private List<Product> products;
 }
