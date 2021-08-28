c9a4e8f7f1b6472400c35a801c9a1a26e8d720d5
Seda Nur Gülocak <sedanurgulocak@gmail.com>
2021-08-28 22:57:27 +0300

    🔨Add validation annotations

--- northwind/src/main/java/kodlamaio/northwind/core/entities/User.java
+++ northwind/src/main/java/kodlamaio/northwind/core/entities/User.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,8 +28,12 @@ public class User {
 	
 	@Column(name="email")
 	@Email
+	@NotBlank
+	@NotNull
 	private String email;
 	
 	@Column(name="password")
+	@NotBlank
+	@NotNull
 	private String password;
 }
